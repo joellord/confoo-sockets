51d471f4a3d393260fbe823595d2432c7d2d542e
Joel Lord <joelphy@gmail.com>
2016-02-26 12:02:27 -0500

    Update twitterCredentials.js

--- demo4/server/twitterCredentials.js
+++ demo4/server/twitterCredentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: "7TfY2MCekYTBz9xMUbyaF2Yqa",
-    consumer_secret: "9GNAoEGAcp6JzgrbiSVvb4SNtj7Kkfl7iYrPkAB009EWZgSdvt",
-    access_token: "820037342-76EPEsvAorIrGGxokgxH6aQe1gNobsDFWPW3AXfX",
-    access_token_secret: "FDVCjPKwgAiLfFTUuXOkDeDsajbLBp2TBG3MUedL1srsO"
+    consumer_key: "XXX",
+    consumer_secret: "XXX",
+    access_token: "XXX-XXX",
+    access_token_secret: "XXX"
 };
